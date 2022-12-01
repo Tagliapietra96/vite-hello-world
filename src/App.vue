ed6554db834e550b069a3f0305fc3401117e1c14
Tagliapietra96 <tagliatek96@gmail.com>
2022-12-01 13:46:48 +0100

    created title

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
 
+export default{
+  components: {AppTitle}
+}
 </script>
 
 <template>
-
+<div class="vh-100 vw-100 d-flex justify-content-center align-items-center">
+  <div class="container border rounded-5 h-75">
+    <AppTitle></AppTitle>
+  </div>
+</div>
 </template>
 
-<style scoped>
+<style>
 
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="text-center mt-5">La mia prima app con Vite!</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+</style>
